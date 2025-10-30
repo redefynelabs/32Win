d62f5d36aa345e16f18810b63f36613473ddb4c8
Manoj Kumar <manojkumararumainathan@gmail.com>
2025-10-30 16:08:14 +0530

    Lotto:web - Starter Template with assets, fonts, themes

---
+++ lotto-web/providers/ScrollProvider.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { ReactNode, useEffect } from "react";
+import Lenis from "lenis";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+interface ScrollProviderProps {
+  children: ReactNode;
+}
+
+export const ScrollProvider = ({ children }: ScrollProviderProps) => {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      smoothWheel: true,
+      touchMultiplier: 1.2,
+    });
+
+    // 🌀 Integrate Lenis with GSAP ScrollTrigger
+    lenis.on("scroll", ScrollTrigger.update);
+
+    gsap.ticker.add((time) => {
+      lenis.raf(time * 1000);
+    });
+
+    gsap.ticker.lagSmoothing(0); // disable GSAP lag smoothing for scroll consistency
+
+    return () => {
+      gsap.ticker.remove((time) => lenis.raf(time * 1000));
+      lenis.destroy();
+    };
+  }, []);
+
+  return <>{children}</>;
+};
